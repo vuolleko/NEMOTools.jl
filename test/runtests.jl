cc2c158aab4cb057c47b1a12e0e8122fdd13c541
Henri Vuollekoski <henri.vuollekoski@fmi.fi>
2023-10-05 11:10:10 +0300

    Add test for min2zero

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using NEMOTools
 using Test
 
 @testset "NEMOTools.jl" begin
-    # Write your tests here.
+    # https://julialang.org/contribute/developing_package/#step_4_test_your_package
+    @test NEMOTools.min2zero([-2, 3, 0, -10, 10]) == [0, 3, 0, 0, 10]
 end
